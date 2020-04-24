0b9d6c24e0b91615f39e072a057cad522c2cac4b
frsty <wladimirburgos42@gmail.com>
2020-04-24 01:59:56 -0400

    models agregado y migrado

--- salon/core/models.py
+++ salon/core/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=200)
+    precio = models.IntegerField()
 
+class Reserva(models.Model):
+    nombre = models.CharField(max_length=200)
+    telefono = models.IntegerField()
+    email = models.EmailField()
+    hora = models.TimeField()
+    fecha = models.DateField()
+    servi = models.CharField(Servicio, max_length=200)
+    
